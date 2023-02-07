212f8cf5969ae364f31a344875643d4a59d5b733
Evelyn <evelia7@gmail.com>
2023-02-06 22:42:16 -0600

    adding arrays, methods, filters, and object without errors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // v1
-
-let wantToBuy = '';
+/* 
+ let wantToBuy = '';
     let buyAgain = '';
     wantToBuy = prompt('¿Quieres comprar un boleto? yes/no');
     if (wantToBuy == 'yes') {
@@ -12,10 +12,9 @@ let wantToBuy = '';
             showData(cost);
             let costOnTreeMonths = cost / 3;
             let costOnSixMonths = cost / 6;
-            alert('Con pago a meses, te quedaria en:  3 meses = ' + costOnTreeMonths + ' o 6 meses = ' + costOnSixMonths);
+            alert('Con pago a meses, te quedaria en:  3 meses = ' + Math.round(costOnTreeMonths) + ' o 6 meses = ' + Math.round(costOnSixMonths));
             let monthsToPay = prompt('Seleccione los meses a pagar: (3) o (6):');
-            alert(name + ' has comprado un boleto a' + monthsToPay + ' mensualidades de ' + cost / monthsToPay
-                + ' y un total de ' + cost);
+            alert(name + ' has comprado un boleto a ' + monthsToPay + ' mensualidades de ' + cost / monthsToPay + ' y un costo total de $' + cost);
             buyAgain = prompt('Comprar otro boleto?? yes/no');
         } while (buyAgain == 'yes');
     }
@@ -24,3 +23,45 @@ let wantToBuy = '';
     function showData(data){
         console.log(data);
     }
+*/
+
+//v2
+
+// Constuctor
+class EventoMusical {
+    constructor(nombre, fecha, costo, lugar, ciudad){
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.costo = costo;
+        this.lugar = lugar;
+        this.ciudad = ciudad;
+    }
+}
+
+// Array para almacenar 
+const eventosMusicales = [] ; 
+
+// Arrays de eventos musicales existentes
+eventosMusicales.push (new EventoMusical ("Metallica", "11 de Febrero 2023", 3000, "Foro Sol", "Ciudad de México"));
+eventosMusicales.push (new EventoMusical ("Guns N' Roses", "25 de Abril 2023", 1200, "Palacio de los Deportes", "Puebla"));
+eventosMusicales.push (new EventoMusical ("The Cranberries", "18 de Septiembre 2023", 2800, "Autodromo", "Guadalajara"));
+eventosMusicales.push (new EventoMusical ("Linkin Park", "21 de Enero 2024", 1600, "Auditorio Nacional", "Monterrey"));
+eventosMusicales.push (new EventoMusical ("Foo Fighters", "8 de Diciembre 2024", 3300, "Foro Sol", "Ciudad de México"));
+
+// console.log(eventosMusicales);
+
+
+
+eventosMusicales.sort((a,b) => {
+    return a.costo - b.costo;
+});
+
+console.log(eventosMusicales);
+
+
+
+//Funcion ORDENAR
+//  function ordenar() {
+      
+//  }
+
